82dfae49690d273c2814e046e2bee5775d80d709
IrinAS <saviris@yandex.ru>
2022-11-28 21:21:14 +0300

    Делали приветствие Маше

---
+++ Example_HelloConsole/example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователья");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("УРААА, это же Маша");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
